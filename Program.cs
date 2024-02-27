2c6b1d51fc8f5bec6cd70ea864c291a6e8f3a0cc
Ajax <apoorv9496@gmail.com>
2024-02-27 20:22:43 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Vadapav;
+
+try
+{
+    Console.WriteLine("Hello, World!");
+
+    var client = new VadapavClient("https://vadapav.mov");
+    var outputPath = "./Downloads";
+    var directoryId = "821f5ea6-b351-4ad9-8cd3-45f245e404ef";
+
+    var directory = await client.GetDirectoryAsync(directoryId);
+    Console.WriteLine($"Selected directory name: {directory.Name}");
+
+    await client.DownloadDirectoryAsync("821f5ea6-b351-4ad9-8cd3-45f245e404ef", outputPath); 
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
